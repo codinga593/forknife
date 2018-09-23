e7641fac8f31ea04c044eef1920008ee1ae01654
Guy <guysayfan@gmail.com>
2018-09-23 13:37:59 -0700

    Sprite stuff

--- game.py
+++ game.py
@@ -1,9 +1,9 @@
 import os
 import pygame.constants
 from constants import *
+from player import Player
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
-
 
 pygame.init()
 
@@ -12,16 +12,19 @@ pygame.display.set_caption('Forknife')
 clock = pygame.time.Clock()
 gameover = False
 
-
+# create player
+p = Player()
+all_sprites = pygame.sprite.Group()
+all_sprites.add(p)
 # main loop
 while not gameover:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:  # This would be a quit event.
             gameover = True
     # Drawing
-    #screen.blit(background, (0, 0))
+    # screen.blit(background, (0, 0))
     screen.fill(cyan)
-    pygame.draw.circle(screen, white, (100, 100), 50)
+    all_sprites.draw(screen)
     pygame.display.update()
     clock.tick(90)
 
