ae3b8f04eaf87834c9e576bc0a16db52360258ee
Gigi <gigi.sayfan@helix.com>
2018-09-23 11:34:44 -0700

    Initial commit

---
+++ constants.py
@@ -0,0 +1,19 @@
+import pygame
+
+screen_width = 800
+screen_height = 600
+font_name = "plump"
+font_size = 25
+
+black = (0, 0, 0)
+gray = (210, 210, 210)
+white = (255, 255, 255)
+cyan = (175, 231, 231)
+laser_red = (255, 36, 36)
+lime_green = (0, 245, 130)
+chill_blue = (50, 145, 255)
+mellow_yellow = (255, 255, 124)
+voila_violet = (204, 153, 255)
+rad_pink = (255, 76, 116)
+
+#background = pygame.image.load('images/background.jpg')
---
+++ game.py
@@ -0,0 +1,109 @@
+import os
+import random
+
+import pygame.constants
+from constants import *
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Forknife')
+clock = pygame.time.Clock()
+gameover = False
+should_play_countdown = True
+
+
+def play_countdown():
+    play_sfx('countdown1edit', 3)
+    play_sfx('countdown2edit')
+
+
+row_count = 5
+
+bricks = []
+
+
+def populate_bricks():
+    for row in range(row_count):
+        for i in range(brick_count):
+            nmbr = random.randint(1, 12)
+            color = white
+            effect = ''
+            if nmbr == 8:
+                color = lime_green
+                effect = 'pdl_grw'
+            elif nmbr == 9:
+                color = chill_blue
+                effect = 'pdl_shrnk'
+            elif nmbr == 10:
+                color = mellow_yellow
+                effect = 'pdl_swft'
+            elif nmbr == 11:
+                color = voila_violet
+                effect = 'pdl_slg'
+            elif nmbr == 12:
+                color = rad_pink
+                effect = 'bll_flckr'
+            brick = (row * (brick_height + 1), offset + i * (brick_width + 1), color, effect)
+            bricks.append(brick)
+
+
+def draw_bricks():
+    for b in bricks:
+        x = b[1]
+        y = b[0]
+        pygame.draw.rect(screen, b[2], (x, y, brick_width, brick_height))
+
+
+populate_bricks()
+
+# main loop
+while state.lives > 0:
+    # Handle key presses
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameover = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                left_down = True
+            if event.key == pygame.K_RIGHT:
+                right_down = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                left_down = False
+            if event.key == pygame.K_RIGHT:
+                right_down = False
+
+    # Move paddle if needed
+    update_ball_y_position()
+    update_ball_x_position()
+    handle_ball_wall_collisions()
+    handle_ball_paddle_collisions(paddle.top, paddle.left, paddle.left + paddle.width, left_down, right_down)
+    handle_ball_brick_collisions(bricks)
+    paddle_left = move_paddle(offset, left_down, right_down,
+                              ball_x - ball_radius, ball_y - ball_radius,
+                              ball_x + ball_radius, ball_y + ball_radius)
+    update_flckr()
+
+
+    # Drawing
+    screen.blit(background, (0, 0))
+    draw_bricks()
+    draw_paddle(screen)
+    draw_ball(screen)
+    render_text(screen, f'Score: {state.score}', score_pos)
+    render_text(screen, f'Lives: {state.lives}', lives_pos)
+    render_text (screen, state.debug, debug_pos)
+
+    pygame.display.update()
+    clock.tick(90)
+
+    if should_play_countdown:
+        pygame.mixer.music.load("SFX/backgroundmusic.ogg")
+        pygame.mixer.music.play(-1)
+        play_countdown()
+        should_play_countdown = False
+pygame.quit()
